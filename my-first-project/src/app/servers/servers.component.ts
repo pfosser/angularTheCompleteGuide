41f1f051598498312d6e1df2d0002ddd86f85fbe
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2023-07-22 12:52:51 +0200

    Lesson 37. Using ngIf to output data conditionally

--- my-first-project/src/app/servers/servers.component.ts
+++ my-first-project/src/app/servers/servers.component.ts
@@ -11,12 +11,14 @@ export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Test server';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => (this.allowNewServer = true), 2000);
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus =
       'Server was created! Name is ' + this.serverName;
   }
