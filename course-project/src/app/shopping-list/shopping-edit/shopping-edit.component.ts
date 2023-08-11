ee756d91c9c6bc8fafcacef0d7e9e9e65420acbd
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2023-08-11 16:49:54 +0200

    Lesson 89. Allowing the user to add ingredients

--- course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput')
+  nameInput!: ElementRef;
 
+  @ViewChild('amountInput')
+  amountInput!: ElementRef;
+
+  @Output()
+  ingredientAdded = new EventEmitter<Ingredient>();
+
+  onAddItem() {
+    const name = this.nameInput.nativeElement.value;
+    const amount = this.amountInput.nativeElement.value;
+
+    this.ingredientAdded.emit(new Ingredient(name, amount));
+  }
 }
