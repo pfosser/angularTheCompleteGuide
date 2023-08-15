82a44bb333c7fde50d1b18fbca8dbba3b7ee2954
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2023-08-15 16:48:52 +0200

    Lesson 109. Creating a data service

---
+++ services-start/src/app/accounts.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+export class AccountsService {
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active',
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive',
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown',
+    },
+  ];
+
+  constructor() {}
+
+  addAccount(name: string, status: string) {
+    this.accounts.push({ name, status });
+  }
+
+  updateStatus(id: number, status: string) {
+    this.accounts[id].status = status;
+  }
+}
