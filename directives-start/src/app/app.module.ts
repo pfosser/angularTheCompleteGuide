869874c73489ff865019bfac45f8799f88c89533
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2023-08-15 11:23:25 +0200

    Lesson 100. Building a structural directive

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
-  declarations: [AppComponent, BasicHighlightDirective, BetterHighlightDirective],
+  declarations: [AppComponent, BasicHighlightDirective, BetterHighlightDirective, UnlessDirective],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
