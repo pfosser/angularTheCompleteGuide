8498723cf71b029b51e16ed146cc97eacfa5306c
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2023-08-15 17:23:08 +0200

    Lesson 112. Injecting Services into Services

--- services-start/src/app/app.module.ts
+++ services-start/src/app/app.module.ts
@@ -1,23 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
-
-import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
+import { AccountsService } from './accounts.service';
+import { AppComponent } from './app.component';
+import { LoggingService } from './logging.service';
 import { NewAccountComponent } from './new-account/new-account.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AccountComponent,
-    NewAccountComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, AccountComponent, NewAccountComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [AccountsService, LoggingService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
