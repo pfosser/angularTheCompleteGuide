7db07398af17da9ed5256e4fe3a45d2a341f90f4
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2023-08-25 17:42:57 +0200

    Lesson 141. Using query params

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -12,6 +12,7 @@ export class ServerComponent implements OnInit {
   server!: { id: number; name: string; status: string } | undefined;
 
   constructor(
+    private router: Router,
     private route: ActivatedRoute,
     private serversService: ServersService
   ) {}
@@ -23,5 +24,9 @@ export class ServerComponent implements OnInit {
       (params: Params) =>
         (this.server = this.serversService.getServer(+params['id']))
     );
+  }
+
+  onEdit() {
+    this.router.navigate(['edit'], { relativeTo: this.route });
   }
 }
