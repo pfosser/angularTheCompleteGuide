c8a975ffefc1fefc3c5951633652601734bcbc84
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2023-08-27 13:29:10 +0200

    Lesson 147. Protecting routes with CanActivate

---
+++ routing-start/src/app/auth.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable, inject } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private authService: AuthService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return this.authService.isAuthenticated().then((auth) => {
+      if (auth) return true;
+      else {
+        this.router.navigate(['/']);
+        return false;
+      }
+    });
+  }
+}
