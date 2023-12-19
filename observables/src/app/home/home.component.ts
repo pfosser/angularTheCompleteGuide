0ba72487f47893a45cd8ddb95cfa41da2e3ec242
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2023-12-19 17:10:02 +0100

    Lesson 178. Errors & Completion

--- observables/src/app/home/home.component.ts
+++ observables/src/app/home/home.component.ts
@@ -18,12 +18,23 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count++);
+        if (count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3!'));
+        }
       }, 1000);
     });
 
-    this.firstObsSub = customIntervalObservable.subscribe((count) =>
-      console.log(count)
-    );
+    this.firstObsSub = customIntervalObservable.subscribe({
+      next: (count) => console.log(count),
+      error: (error) => {
+        console.log(error);
+        alert(error);
+      },
+      complete: () => console.log('Completed'),
+    });
   }
 
   ngOnDestroy(): void {
