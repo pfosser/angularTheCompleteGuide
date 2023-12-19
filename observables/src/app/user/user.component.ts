271204376a775ac7dca332b08d2e7dd9ec27f21f
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2023-12-19 17:35:49 +0100

    Lesson 181. Subjects. Old approach

--- observables/src/app/user/user.component.ts
+++ observables/src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user',
@@ -9,7 +10,10 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class UserComponent implements OnInit {
   id!: number;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
     // All the observables provided by angular are managed by the framework and
@@ -17,5 +21,9 @@ export class UserComponent implements OnInit {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
     });
+  }
+
+  onActivate() {
+    this.userService.activatedEmitter.emit(true);
   }
 }
