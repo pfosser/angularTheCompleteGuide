6b185f1b05c93bb049a4400275921f24dfdf2999
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2023-12-19 17:54:36 +0100

    Lesson 185. Changed the Subscription Name

--- course-project/src/app/recipes/recipes.component.ts
+++ course-project/src/app/recipes/recipes.component.ts
@@ -9,13 +9,7 @@ import { RecipeService } from './recipe.service';
   providers: [RecipeService],
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe | undefined;
-
   constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe) => (this.selectedRecipe = recipe)
-    );
-  }
+  ngOnInit(): void {}
 }
