5c7d6dc44d7b783c6967c022d7e82ed7390cff23
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2023-12-19 17:25:16 +0100

    Lesson 180. Understanding Operators

--- observables/src/app/home/home.component.ts
+++ observables/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription, interval } from 'rxjs';
+import { Observable, Subscription, interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -14,27 +14,31 @@ export class HomeComponent implements OnInit, OnDestroy {
     // this.firstObsSub = interval(1000).subscribe((count) => {
     //   console.log(count);
     // });
-    const customIntervalObservable = new Observable((observer) => {
-      let count = 0;
-      setInterval(() => {
-        observer.next(count++);
-        if (count === 2) {
-          observer.complete();
-        }
-        if (count > 3) {
-          observer.error(new Error('Count is greater than 3!'));
-        }
-      }, 1000);
-    });
+    const customIntervalObservable: Observable<number> = new Observable(
+      (observer) => {
+        let count = 0;
+        setInterval(() => {
+          observer.next(count++);
+          if (count === 2) {
+            observer.complete();
+          }
+          if (count > 3) {
+            observer.error(new Error('Count is greater than 3!'));
+          }
+        }, 1000);
+      }
+    );
 
-    this.firstObsSub = customIntervalObservable.subscribe({
-      next: (count) => console.log(count),
-      error: (error) => {
-        console.log(error);
-        alert(error);
-      },
-      complete: () => console.log('Completed'),
-    });
+    this.firstObsSub = customIntervalObservable
+      .pipe(map((data: number) => `Round ${data + 1}`))
+      .subscribe({
+        next: (count) => console.log(count),
+        error: (error) => {
+          console.log(error);
+          alert(error);
+        },
+        complete: () => console.log('Completed'),
+      });
   }
 
   ngOnDestroy(): void {
