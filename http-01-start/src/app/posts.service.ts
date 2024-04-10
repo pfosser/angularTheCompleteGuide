94a8d3526e784b4109ec93de48efdaba77039b81
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-04-10 08:42:48 +0200

    Lesson 273. Adding Query Params

--- http-01-start/src/app/posts.service.ts
+++ http-01-start/src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Post } from './post.model';
 import { Subject, catchError, map, throwError } from 'rxjs';
@@ -28,11 +28,15 @@ export class PostsService {
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('custom', 'key');
     return this.http
       .get<Record<string, Post>>(
         'https://ng-complete-guide-28b6c-default-rtdb.firebaseio.com/posts.json',
         {
           headers: new HttpHeaders({ 'Custom-Header': 'Hello' }),
+          params: searchParams,
         }
       )
       .pipe(
