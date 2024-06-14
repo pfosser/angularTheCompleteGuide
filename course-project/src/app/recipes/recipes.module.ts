e7a7468cbf1f3bd843dddbf4387911768befc31d
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-06-14 08:45:27 +0200

    Lesson 411. Splitting Modules Correctly

--- course-project/src/app/recipes/recipes.module.ts
+++ course-project/src/app/recipes/recipes.module.ts
@@ -1,15 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
-import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import { RecipesComponent } from './recipes.component';
-import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
-import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipesComponent } from './recipes.component';
+
 @NgModule({
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   declarations: [
     RecipesComponent,
     RecipeListComponent,
