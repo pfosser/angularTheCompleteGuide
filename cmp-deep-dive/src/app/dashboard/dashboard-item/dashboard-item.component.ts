30781e2bd729592128d787a0d72af4bb38c8d189
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-08-26 18:29:04 +0200

    Lesson 99. Component Inputs: Repetition

--- cmp-deep-dive/src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ cmp-deep-dive/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
   standalone: true,
   imports: [],
   templateUrl: './dashboard-item.component.html',
-  styleUrl: './dashboard-item.component.css'
+  styleUrl: './dashboard-item.component.css',
 })
 export class DashboardItemComponent {
+  image = input.required<{ src: string; alt: string }>();
 
+  title = input.required<string>();
 }
