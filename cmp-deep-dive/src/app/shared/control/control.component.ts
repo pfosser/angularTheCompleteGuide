077dacec7e5364154ef018a21ce286c9a5f80580
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-09-13 08:40:50 +0200

    Lesson 116. Accessing Host Elements Programmatically

--- cmp-deep-dive/src/app/shared/control/control.component.ts
+++ cmp-deep-dive/src/app/shared/control/control.component.ts
@@ -1,4 +1,12 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -7,10 +15,27 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
+  // This should be the preferred way now
   host: {
     class: 'control',
+    '(click)': 'onClick()',
   },
 })
 export class ControlComponent {
+  // This now exists for compatibility reasons
+  // @HostBinding('class') className = 'control';
+
   label = input.required<string>();
+
+  // ElementRef is a generic type that can refer to any element
+  // on the page, but by injecting it into a component like this,
+  // Angular will give you access to the host element of that
+  // component
+  private el = inject(ElementRef);
+
+  // @HostListener('click')
+  onClick() {
+    console.log('Clicked!');
+    console.log(this.el);
+  }
 }
