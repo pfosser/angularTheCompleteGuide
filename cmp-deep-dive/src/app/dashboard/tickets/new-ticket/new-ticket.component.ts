954b8123cf79b4db740734a4a23bfad879711899
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-09-30 08:44:03 +0200

    Lesson 128. Extracting Input Values via Template Variables

--- cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -11,7 +11,8 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
-  onSubmit(titleElement: HTMLInputElement) {
-    console.log('Entered title: ' + titleElement.value);
+  onSubmit(title: string, text: string) {
+    console.log('Entered title: ' + title);
+    console.log('Entered text: ' + text);
   }
 }
