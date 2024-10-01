e4f68d1355dd8b388f24569696515d671d9cee66
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-10-01 08:45:13 +0200

    Lesson 130. Getting Access to Template Elements via ViewChild

--- cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { FormsModule } from '@angular/forms';
@@ -11,8 +11,14 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
+  // the selector can be a template variable
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+
+  // one possibility is to pass the form through a template variable
+  // onSubmit(title: string, text: string, form: HTMLFormElement) {
   onSubmit(title: string, text: string) {
     console.log('Entered title: ' + title);
     console.log('Entered text: ' + text);
+    this.form?.nativeElement.reset();
   }
 }
