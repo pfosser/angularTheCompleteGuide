247b1a20a9bbdb042edf8dd7c7752961219e73ce
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-10-22 08:57:05 +0200

    Lesson 145. Setting Up Custom Two-Way Binding

--- custom-2w-binding/src/app/rect/rect.component.ts
+++ custom-2w-binding/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -8,9 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './rect.component.css',
 })
 export class RectComponent {
-  // Todo: Implement custom two-way binding
+  // size = input.required<{ width: string; height: string }>();
+  @Input() size!: { width: string; height: string };
+
+  // sizeChanged = output<{ width: string; height: string }>();
+  @Output() sizeChange = new EventEmitter<{ width: string; height: string }>();
 
   onReset() {
-    // ...
+    this.sizeChange.emit({
+      width: '200',
+      height: '100',
+    });
   }
 }
