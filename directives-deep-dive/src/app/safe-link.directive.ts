fa23df06d93732d5f88815ba15522c831d79129e
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-11-02 19:16:12 +0100

    Lesson 154. Working with Inputs in Custom Directives (simple)

--- directives-deep-dive/src/app/safe-link.directive.ts
+++ directives-deep-dive/src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, input } from '@angular/core';
 
 @Directive({
   selector: 'a[appSafeLink]',
@@ -8,13 +8,21 @@ import { Directive } from '@angular/core';
   },
 })
 export class SafeLinkDirective {
+  queryParam = input('myapp');
+
   constructor() {
     console.log('SageLinkDirective is active');
   }
 
   onConfirmLeavePage(event: MouseEvent) {
-    if (!window.confirm('Do you want to leave the app?')) {
-      event.preventDefault();
+    const wantsToLeave = window.confirm('Do you want to leave the app?');
+
+    if (wantsToLeave) {
+      const address = (event.target as HTMLAnchorElement).href;
+      (event.target as HTMLAnchorElement).href =
+        address + '?from=' + this.queryParam();
+      return;
     }
+    event.preventDefault();
   }
 }
