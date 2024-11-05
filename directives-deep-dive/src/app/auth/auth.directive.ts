797991e98b5cb3375ef6a02a11e39330ab181114
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-11-05 08:52:10 +0100

    Lesson 157. Building a Custom Structural Directive

--- directives-deep-dive/src/app/auth/auth.directive.ts
+++ directives-deep-dive/src/app/auth/auth.directive.ts
@@ -1,4 +1,11 @@
-import { Directive, effect, inject, input } from '@angular/core';
+import {
+  Directive,
+  effect,
+  inject,
+  input,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
 import { Permission } from './auth.model';
 import { AuthService } from './auth.service';
 
@@ -9,14 +16,26 @@ import { AuthService } from './auth.service';
 export class AuthDirective {
   userType = input.required<Permission>({ alias: 'appAuth' });
 
-  authService = inject(AuthService);
+  private authService = inject(AuthService);
+
+  /* when injecting a templateref, you tell Angular
+     that this directive will be used on an ng-template element
+     and that you wanna get hold of that template and
+     therefore implicitly also of the content inside of
+     that template, which by the way doesn't have
+     to be a single element. */
+  private templateRef = inject(TemplateRef);
+
+  /* This view container ref in the end is a reference
+     to the place in the DOM where this template is being used. */
+  private viewContainerRef = inject(ViewContainerRef);
 
   constructor() {
     effect(() => {
       if (this.authService.activePermission() === this.userType()) {
-        console.log('SHOW ELEMENT');
+        this.viewContainerRef.createEmbeddedView(this.templateRef);
       } else {
-        console.log('DO NOT SHOW ELEMENT');
+        this.viewContainerRef.clear();
       }
     });
   }
