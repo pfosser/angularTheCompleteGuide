0960f3ff3199d41a393a747c0f4c640501905ecc
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-11-07 08:44:45 +0100

    Lesson 159. Naive use of directive in multiple places

--- directives-deep-dive/src/app/app.component.ts
+++ directives-deep-dive/src/app/app.component.ts
@@ -5,13 +5,20 @@ import { AuthComponent } from './auth/auth.component';
 import { AuthDirective } from './auth/auth.directive';
 import { AuthService } from './auth/auth.service';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
+import { LogDirective } from './log.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent, NgIf, AuthDirective],
+  imports: [
+    AuthComponent,
+    LearningResourcesComponent,
+    NgIf,
+    AuthDirective,
+    LogDirective,
+  ],
 })
 export class AppComponent {
   private authService = inject(AuthService);
