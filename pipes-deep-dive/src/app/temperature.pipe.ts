e5541da8eafde00f8361063643614ab2a465c58d
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-11-12 10:21:53 +0100

    Lesson 166. Accepting Parameters in Custom Pipes

--- pipes-deep-dive/src/app/temperature.pipe.ts
+++ pipes-deep-dive/src/app/temperature.pipe.ts
@@ -5,15 +5,32 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class TemperaturePipe implements PipeTransform {
-  transform(value: string | number): string {
+  transform(
+    value: string | number,
+    inputType: 'cel' | 'fah',
+    outputType?: 'cel' | 'fah'
+  ): string {
     let val: number;
     if (typeof value === 'string') {
       val = parseFloat(value);
     } else {
       val = value;
     }
-    const outTemp = val * (9 / 5) + 32;
+    let outTemp: number;
+    if (inputType === 'cel' && outputType === 'fah') {
+      outTemp = val * (9 / 5) + 32;
+    } else if (inputType === 'fah' && outputType === 'cel') {
+      outTemp = (val - 32) * (5 / 9);
+    } else {
+      outTemp = val;
+    }
 
-    return `${outTemp} °F`;
+    let symbol: 'C' | 'F';
+    if (!outputType) {
+      symbol = inputType === 'cel' ? 'C' : 'F';
+    } else {
+      symbol = outputType === 'cel' ? 'C' : 'F';
+    }
+    return `${outTemp} °${symbol}`;
   }
 }
