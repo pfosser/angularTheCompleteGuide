a1a80d90c833fda6ce8b83b6e99ee55c44f90be2
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-11-25 08:52:29 +0100

    Lesson 180. There Are Multiple Ways Of Providing a Service

--- services-and-di-deep-dive/src/main.ts
+++ services-and-di-deep-dive/src/main.ts
@@ -2,4 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
+// Alternative way to provide an injectable with the Application root environment injector.
+// This approach doesn't permit tree-shaking of the injectable, so annotating with
+// @Injectable() is the preferred way
+// bootstrapApplication(AppComponent, {
+//   providers: [TasksService],
+// }).catch((err) => console.error(err));
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
