7d1b83145341a799e85ef79a4778facf1150a077
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-11-26 08:42:02 +0100

    Lesson 181. Providing Services via the Element Injector

--- services-and-di-deep-dive/src/app/tasks/tasks.component.ts
+++ services-and-di-deep-dive/src/app/tasks/tasks.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 
 import { NewTaskComponent } from './new-task/new-task.component';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   imports: [NewTaskComponent, TasksListComponent],
+  // Way of providing something through the element injector.
+  // That something will be available to the element and all
+  // the contained elements (those used in the template).
+  // providers: [TasksService],
 })
 export class TasksComponent {}
