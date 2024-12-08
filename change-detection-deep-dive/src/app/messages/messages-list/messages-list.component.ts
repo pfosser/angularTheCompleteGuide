7be61c09fbc6a2e11dd8d2181377c130cf5b4b32
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-12-08 10:42:27 +0100

    Lesson 202. Introducing The async Pipe

--- change-detection-deep-dive/src/app/messages/messages-list/messages-list.component.ts
+++ change-detection-deep-dive/src/app/messages/messages-list/messages-list.component.ts
@@ -1,11 +1,5 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  DestroyRef,
-  inject,
-  OnInit,
-} from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MessagesService } from '../messages.service';
 
 @Component({
@@ -14,29 +8,14 @@ import { MessagesService } from '../messages.service';
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [AsyncPipe],
 })
-export class MessagesListComponent implements OnInit {
+export class MessagesListComponent {
   private messagesService = inject(MessagesService);
-  private cdRef = inject(ChangeDetectorRef);
-
-  private destroyRef = inject(DestroyRef);
-
-  messages: string[] = [];
+  messages$ = this.messagesService.messages$;
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
     return 'MessagesList Component Debug Output';
-  }
-
-  ngOnInit(): void {
-    const subscription = this.messagesService.messages$.subscribe(
-      (messages) => {
-        this.messages = messages;
-        this.cdRef.markForCheck();
-      }
-    );
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
-    });
   }
 }
