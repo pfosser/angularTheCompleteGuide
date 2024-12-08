0ed71a6256d45042daa64ea90aefe76f14e7eda3
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-12-08 10:36:22 +0100

    Lesson 201. Triggering Change Detection Manually & Using RxJS Subjects

--- change-detection-deep-dive/src/app/messages/messages-list/messages-list.component.ts
+++ change-detection-deep-dive/src/app/messages/messages-list/messages-list.component.ts
@@ -1,8 +1,13 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { MessagesService } from '../messages.service';
 
-// With this setup (no signals and OnPush), the addition of a message
-// doesn't trigger a cd and the new message doesn't show up in the list
 @Component({
   selector: 'app-messages-list',
   standalone: true,
@@ -10,14 +15,28 @@ import { MessagesService } from '../messages.service';
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MessagesListComponent {
+export class MessagesListComponent implements OnInit {
   private messagesService = inject(MessagesService);
-  get messages() {
-    return this.messagesService.allMessages;
-  }
+  private cdRef = inject(ChangeDetectorRef);
+
+  private destroyRef = inject(DestroyRef);
+
+  messages: string[] = [];
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
     return 'MessagesList Component Debug Output';
+  }
+
+  ngOnInit(): void {
+    const subscription = this.messagesService.messages$.subscribe(
+      (messages) => {
+        this.messages = messages;
+        this.cdRef.markForCheck();
+      }
+    );
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
   }
 }
