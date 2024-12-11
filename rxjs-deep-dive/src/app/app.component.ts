c93708f94739335d336a8a83bbff2df8d9fd030f
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-12-11 08:46:40 +0100

    Lesson 211. Converting Observables To Signals

--- rxjs-deep-dive/src/app/app.component.ts
+++ rxjs-deep-dive/src/app/app.component.ts
@@ -6,7 +6,8 @@ import {
   OnInit,
   signal,
 } from '@angular/core';
-import { toObservable } from '@angular/core/rxjs-interop';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -17,6 +18,21 @@ export class AppComponent implements OnInit {
   clickCount = signal(0);
   // toObservable can be used only in injection contexts
   clickCount$ = toObservable(this.clickCount);
+
+  interval$ = interval(1000);
+
+  // toSignal too can be used only in injection contexts
+  // running the app you can see that initially there is no value:
+  // it is a difference between signal and observable.
+  // Angular gives the undefined value to a signal created in this way,
+  // unless you specify one in the configuration object.
+  // The unsubscription to the original observable is managed by Angular
+  // unless you specify differently in the config obj.
+  intervalSignal = toSignal(this.interval$, {
+    initialValue: 42,
+    // manualCleanup: true,
+  });
+
   private destroyRef = inject(DestroyRef);
 
   constructor() {
