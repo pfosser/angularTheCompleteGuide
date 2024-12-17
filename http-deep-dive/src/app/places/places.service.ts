cccb4ee06fd07b233ba827612129e543db236274
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-12-17 11:46:32 +0100

    Lesson 231. Improved Optimistic Updating

--- http-deep-dive/src/app/places/places.service.ts
+++ http-deep-dive/src/app/places/places.service.ts
@@ -33,11 +33,23 @@ export class PlacesService {
   }
 
   addPlaceToUserPlaces(place: Place) {
-    this.userPlaces.update((prevPlaces) => [...prevPlaces, place]);
+    const prevPlaces = this.userPlaces();
 
-    return this.httpClient.put('http://localhost:3000/user-places', {
-      placeId: place.id,
-    });
+    // prevents adding the same place twice
+    if (!prevPlaces.some((p) => p.id === place.id)) {
+      this.userPlaces.set([...prevPlaces, place]);
+    }
+
+    return this.httpClient
+      .put('http://localhost:3000/user-places', {
+        placeId: place.id,
+      })
+      .pipe(
+        catchError(() => {
+          this.userPlaces.set(prevPlaces);
+          return throwError(() => new Error('Failed to store selected place.'));
+        })
+      );
   }
 
   removeUserPlace(place: Place) {}
