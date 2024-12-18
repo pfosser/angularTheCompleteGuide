300148070648db8ea4096caf89f10152364d496e
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-12-18 13:13:30 +0100

    Lesson 241. Extracting User Input Values

--- forms-deep-dive/src/app/auth/login/login.component.ts
+++ forms-deep-dive/src/app/auth/login/login.component.ts
@@ -11,7 +11,9 @@ import { FormsModule, NgForm } from '@angular/forms';
   imports: [FormsModule],
 })
 export class LoginComponent {
-  onSubmit(form: NgForm) {
-    console.log(form);
+  onSubmit(formData: NgForm) {
+    const enteredEmail = formData.form.value.email;
+    const enteredPassword = formData.form.value.password;
+    console.log(enteredEmail, enteredPassword);
   }
 }
