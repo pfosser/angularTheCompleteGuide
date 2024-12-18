9df54e8015907323728a801ef52d90084f19749c
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-12-18 12:59:47 +0100

    Lesson 240. Getting Access to the Angular-managed Form

--- forms-deep-dive/src/app/auth/login/login.component.ts
+++ forms-deep-dive/src/app/auth/login/login.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  // By importing this module, Angular registers a directive having
+  // the form element as a selector, so Angular can control form elements.
   imports: [FormsModule],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
+}
