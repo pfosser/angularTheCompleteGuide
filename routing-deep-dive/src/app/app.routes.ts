a3ad6be1eefb1b928f33639f33764254d38763a9
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-12-23 17:18:10 +0100

    Lesson 266. Registering Multiple Routes

--- routing-deep-dive/src/app/app.routes.ts
+++ routing-deep-dive/src/app/app.routes.ts
@@ -1,6 +1,11 @@
+import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { TasksComponent } from './tasks/tasks.component';
 
 export const routes = [
+  {
+    path: '',
+    component: NoTaskComponent,
+  },
   {
     path: 'tasks', // or <your-domain>/tasks
     component: TasksComponent,
