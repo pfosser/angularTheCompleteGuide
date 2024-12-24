ba701d940bbcb2ec9b1d1f8ee011c0bbb56829ed
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-12-24 17:55:04 +0100

    Lesson 270. Extracting Dynamic Route Parameters via Inputs

--- routing-deep-dive/src/app/app.config.ts
+++ routing-deep-dive/src/app/app.config.ts
@@ -1,7 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  // tells Angular that we want an input binding approach
+  providers: [provideRouter(routes, withComponentInputBinding())],
 };
