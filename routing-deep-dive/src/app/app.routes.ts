64d3eefdd749dd2972b1cab2980418eac779162d
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2024-12-24 18:12:33 +0100

    Lesson 273. Working with Nested Routes

--- routing-deep-dive/src/app/app.routes.ts
+++ routing-deep-dive/src/app/app.routes.ts
@@ -1,4 +1,6 @@
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
+import { TasksComponent } from './tasks/tasks.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 
 export const routes = [
@@ -9,5 +11,15 @@ export const routes = [
   {
     path: 'users/:userId', // <your-domain>/users/<uid>
     component: UserTasksComponent,
+    children: [
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        component: TasksComponent,
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+      },
+    ],
   },
 ];
--- routing-deep-dive/src/app/users/user-tasks/user-tasks.component.ts
+++ routing-deep-dive/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,19 +1,14 @@
-import {
-  Component,
-  computed,
-  DestroyRef,
-  inject,
-  input,
-  OnInit,
-} from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+
 import { UsersService } from '../users.service';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
+  imports: [RouterOutlet],
 })
 export class UserTasksComponent implements OnInit {
   // userId = input.required<string>();
