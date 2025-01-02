d5878608eb20d75a51caaa77168a1c8844e25b96
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-01-02 09:03:30 +0100

    Lesson 279. Redirecting Users

--- routing-deep-dive/src/app/app.routes.ts
+++ routing-deep-dive/src/app/app.routes.ts
@@ -1,10 +1,11 @@
+import { Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 
-export const routes = [
+export const routes: Routes = [
   {
     path: '', // <your-domain>/
     component: NoTaskComponent,
@@ -13,6 +14,15 @@ export const routes = [
     path: 'users/:userId', // <your-domain>/users/<uid>
     component: UserTasksComponent,
     children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        // In our scenario it would not matter if set to prefix or full
+        // prefix means that the combined path (of this one and parent root)
+        // must be a prefix of the url in the browser
+        // full means that they must match exactly
+        pathMatch: 'prefix',
+      },
       {
         path: 'tasks', // <your-domain>/users/<uid>/tasks
         component: TasksComponent,
