fc51cda06456296eb6757a9ae7df0423f18fdbce
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-01-03 08:56:51 +0100

    Lesson 282. Setting Query Parameters

--- routing-deep-dive/src/app/tasks/tasks.component.ts
+++ routing-deep-dive/src/app/tasks/tasks.component.ts
@@ -4,13 +4,14 @@ import { User } from '../users/user/user.model';
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
 import { TasksService } from './tasks.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
+  imports: [TaskComponent, RouterLink],
 })
 export class TasksComponent {
   // By default child routes cannot get parent route parameters through input
