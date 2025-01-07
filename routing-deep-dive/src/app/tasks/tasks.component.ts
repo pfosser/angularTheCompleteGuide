f594651d97ef657a161d3613290f3ee073a5affb
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-01-07 08:51:36 +0100

    Lesson 284. Extracting Query Parameters via Observables

--- routing-deep-dive/src/app/tasks/tasks.component.ts
+++ routing-deep-dive/src/app/tasks/tasks.component.ts
@@ -1,10 +1,17 @@
-import { Component, computed, inject, input } from '@angular/core';
+import {
+  Component,
+  computed,
+  DestroyRef,
+  inject,
+  input,
+  OnInit,
+} from '@angular/core';
 
 import { User } from '../users/user/user.model';
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
 import { TasksService } from './tasks.service';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
@@ -13,18 +20,32 @@ import { RouterLink } from '@angular/router';
   styleUrl: './tasks.component.css',
   imports: [TaskComponent, RouterLink],
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
   // By default child routes cannot get parent route parameters through input
   // binding (only through ActivatedRoute).
   // To overome the limitation you have to explicitly configure it (see app.config.ts)
   userId = input.required<string>();
 
   // Query parametere binding
-  order = input<'asc' | 'desc'>();
+  // order = input<'asc' | 'desc'>();
+
+  order?: 'asc' | 'desc';
 
   private tasksService = inject(TasksService);
+
+  private destroyRef = inject(DestroyRef);
 
   userTasks = computed(() =>
     this.tasksService.allTasks().filter((t) => t.userId === this.userId()),
   );
+
+  private activatedRoute = inject(ActivatedRoute);
+
+  ngOnInit() {
+    const subscription = this.activatedRoute.queryParams.subscribe({
+      next: (params) => (this.order = params['order']),
+    });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 }
