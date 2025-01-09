d49452bd076a9c57165936d624576740fc41fd8c
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-01-09 10:09:25 +0100

    Lesson 287. Resolving Route-related Dynamic Data

--- routing-deep-dive/src/app/app.routes.ts
+++ routing-deep-dive/src/app/app.routes.ts
@@ -2,7 +2,10 @@ import { Routes } from '@angular/router';
 
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import {
+  resolveUserName,
+  UserTasksComponent,
+} from './users/user-tasks/user-tasks.component';
 import { routes as userRoutes } from './users/users.routes';
 
 export const routes: Routes = [
@@ -16,6 +19,18 @@ export const routes: Routes = [
     children: userRoutes,
     data: {
       message: 'Hello!',
+    },
+    // this resolver will be called for every navigation action.
+    // So every time this route gets active, this resolver function will be called
+    // and it will then receive the latest snapshot that describes this activated route.
+    // That's why there is no reason to subscribe to it here because it will be re executed.
+    // This function will be re executed whenever the route becomes active,
+    // even if it already was active.  So it will be called again if the route parameter
+    // changes, for example.
+    resolve: {
+      // The value returned by the function will be made available to the component
+      // in an input with the name of the key, thanks to route parameter binding
+      userName: resolveUserName,
     },
   },
   {
--- routing-deep-dive/src/app/users/user-tasks/user-tasks.component.ts
+++ routing-deep-dive/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,5 +1,12 @@
-import { Component, DestroyRef, inject, input, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+import { Component, DestroyRef, inject, input } from '@angular/core';
+import {
+  ActivatedRoute,
+  ActivatedRouteSnapshot,
+  ResolveFn,
+  RouterLink,
+  RouterOutlet,
+  RouterStateSnapshot,
+} from '@angular/router';
 
 import { UsersService } from '../users.service';
 
@@ -10,33 +17,22 @@ import { UsersService } from '../users.service';
   styleUrl: './user-tasks.component.css',
   imports: [RouterOutlet, RouterLink],
 })
-export class UserTasksComponent implements OnInit {
-  // userId = input.required<string>();
-
-  private usersService = inject(UsersService);
-
-  private activatedRoute = inject(ActivatedRoute);
-
-  private destroyRef = inject(DestroyRef);
-
-  // userName = computed(
-  //   () => this.usersService.users.find((u) => u.id === this.userId())?.name,
-  // );
-  userName = '';
+export class UserTasksComponent {
+  userName = input.required<string>();
 
   // Router parameter binding on the key 'message' of the static data associated
   // with the route
   message = input.required<string>();
-
-  ngOnInit(): void {
-    console.log('Input data :>> ', this.message());
-    const subscription = this.activatedRoute.paramMap.subscribe({
-      next: (paramMap) =>
-        (this.userName =
-          this.usersService.users.find((u) => u.id === paramMap.get('userId'))
-            ?.name || ''),
-    });
-
-    this.destroyRef.onDestroy(() => subscription.unsubscribe());
-  }
 }
+
+export const resolveUserName: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot,
+  routerState: RouterStateSnapshot,
+) => {
+  const usersService = inject(UsersService);
+  const userName =
+    usersService.users.find(
+      (u) => u.id === activatedRoute.paramMap.get('userId'),
+    )?.name || '';
+  return userName;
+};
