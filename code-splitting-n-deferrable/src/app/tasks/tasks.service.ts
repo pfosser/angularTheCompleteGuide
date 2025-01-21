54868a83977d3c6394d26b7e6a22e46f50b7f047
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-01-21 08:55:26 +0100

    Lesson 302. Using Lazy Loading & Routing to Lazy-load Services

--- code-splitting-n-deferrable/src/app/tasks/tasks.service.ts
+++ code-splitting-n-deferrable/src/app/tasks/tasks.service.ts
@@ -2,7 +2,7 @@ import { Injectable, signal } from '@angular/core';
 
 import { type NewTaskData } from './task/task.model';
 
-@Injectable({ providedIn: 'root' })
+@Injectable()
 export class TasksService {
   private tasks = signal([
     {
@@ -56,7 +56,7 @@ export class TasksService {
 
   removeTask(id: string) {
     this.tasks.update((prevTasks) =>
-      prevTasks.filter((task) => task.id !== id)
+      prevTasks.filter((task) => task.id !== id),
     );
     this.saveTasks();
   }
