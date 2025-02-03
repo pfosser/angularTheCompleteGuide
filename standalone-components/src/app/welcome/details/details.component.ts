8663582d3764ace43bf32c24eba23bdbe9548041
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-02-03 14:24:05 +0100

    Lesson 661. Standalone Directives & Connecting Building Blocks

--- standalone-components/src/app/welcome/details/details.component.ts
+++ standalone-components/src/app/welcome/details/details.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
+
 import { AnalyticsService } from 'src/app/shared/analytics.service';
-import { SharedModule } from 'src/app/shared/shared.module';
+import { HighlightDirective } from 'src/app/shared/highlight.directive';
 
 @Component({
   standalone: true,
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css'],
-  imports: [SharedModule],
+  imports: [HighlightDirective],
 })
 export class DetailsComponent {
   constructor(private analyticsService: AnalyticsService) {}
