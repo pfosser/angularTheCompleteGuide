095750d0c49ce600da23b4f9c23c2ff0146254ed
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-02-03 09:00:42 +0100

    Lesson 659. Building a First Standalone Component

--- standalone-components/src/app/welcome/welcome.component.ts
+++ standalone-components/src/app/welcome/welcome.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
-  templateUrl: './welcome.component.html'
+  templateUrl: './welcome.component.html',
+  // the current component must be standalone too for this to work
+  // imports: [DetailsComponent],
 })
 export class WelcomeComponent {}
