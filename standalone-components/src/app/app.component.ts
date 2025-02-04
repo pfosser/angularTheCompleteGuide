d2d61fda5ecf9104f5682a68a9dbedf6495a4195
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-02-04 09:07:38 +0100

    Lesson 665. Routing with Standalone Components

--- standalone-components/src/app/app.component.ts
+++ standalone-components/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 import { WelcomeComponent } from './welcome/welcome.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [WelcomeComponent],
+  imports: [WelcomeComponent, RouterModule],
 })
 export class AppComponent {}
--- standalone-components/src/main.ts
+++ standalone-components/src/main.ts
@@ -1,9 +1,10 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 // import { AnalyticsService } from './app/shared/analytics.service';
 import { environment } from './environments/environment';
+import { AppRoutingModule } from './app/app-routing.module';
 
 if (environment.production) {
   enableProdMode();
@@ -13,5 +14,19 @@ bootstrapApplication(AppComponent, {
   providers: [
     // Equivalent to specifying { providedIn: 'root' } in the service decorator
     //AnalyticsService
+
+    // This makes our standalone root component
+    // aware of our routes and makes the overall
+    // Angular application aware of our routes
+    importProvidersFrom(AppRoutingModule),
   ],
 });
+// So lazy loading works just fine here and it's also worth
+// noting that all of that works in an application
+// in an Angular app where many components including
+// the root component, are actually standalone components.
+// So app component is a standalone component and nonetheless
+// by connecting our routing setup like this, we are able
+// to use routing with our app-routing.module and with a couple
+// of components that are not standalone components at all:
+// dashboard, today, and about are not standalone components.
