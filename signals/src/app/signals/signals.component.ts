a0f6339bdd3880721e25d83a2cb46b6c6fa538ba
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-02-07 08:57:01 +0100

    Lesson 671. Updating a Signal Value

--- signals/src/app/signals/signals.component.ts
+++ signals/src/app/signals/signals.component.ts
@@ -12,12 +12,12 @@ export class SignalsComponent {
   counter = signal(0);
 
   increment() {
-    this.counter++;
+    this.counter.update((oldValue) => ++oldValue);
     this.actions.push('INCREMENT');
   }
 
   decrement() {
-    this.counter--;
+    this.counter.update((oldValue) => --oldValue);
     this.actions.push('DECREMENT');
   }
 }
