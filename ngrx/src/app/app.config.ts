24aaf5d9fb2d2d5d5caa65175f2eb79bcc0dda10
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-02-11 08:42:46 +0100

    Lesson 682. Adding a First Reducer & Store Setup

--- ngrx/src/app/app.config.ts
+++ ngrx/src/app/app.config.ts
@@ -1,9 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 
+import { routes } from './app.routes';
+import { counterReducer } from './store/counter.reducer';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideStore({
+      counter: counterReducer,
+    }),
+  ],
 };
