9def4215002bacbac93182ed6bebf6b6cd66872f
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-02-11 08:56:50 +0100

    Lesson 684. Reading Data From The Store

--- ngrx/src/app/app.component.ts
+++ ngrx/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { CounterControlsComponent } from './counter-controls/counter-controls.co
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
   standalone: true,
   imports: [CounterOutputComponent, CounterControlsComponent],
 })
