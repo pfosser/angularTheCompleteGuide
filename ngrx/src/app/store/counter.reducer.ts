3f0c7cfca109122c8608d5eccc8f633a5a5edf94
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-02-11 09:03:53 +0100

    Lesson 685. Introducing Actions & State Changing Reducers

--- ngrx/src/app/store/counter.reducer.ts
+++ ngrx/src/app/store/counter.reducer.ts
@@ -1,10 +1,14 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
+import { increment } from './counter.actions';
 
 const initialState = 0;
 
-// export const counterReducer = createReducer(initialState);
+export const counterReducer = createReducer(
+  initialState,
+  on(increment, (state) => state + 1)
+);
 
 // Older but still valid way to create reducers
-export function counterReducer(state = initialState) {
-  return state;
-}
+// export function counterReducer(state = initialState) {
+//   return state;
+// }
