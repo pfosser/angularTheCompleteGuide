6b4fd146c998df3c6f55def06cfb4e3035a8b3c5
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-02-12 09:01:35 +0100

    Lesson 689. An Alternative Way Of Defining Actions

--- ngrx/src/app/store/counter.actions.ts
+++ ngrx/src/app/store/counter.actions.ts
@@ -4,3 +4,13 @@ export const increment = createAction(
   '[Counter] Increment',
   props<{ value: number }>()
 );
+
+// Older way of defining actions
+// export const INCREMENT = '[Counter] Increment';
+// export class IncrementAction implements Action {
+//   readonly type = INCREMENT;
+
+//   constructor(public value: number) {}
+// }
+
+// export type CounterActions = IncrementAction;
