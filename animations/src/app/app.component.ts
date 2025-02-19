fa45a01e77c292f9838e1f542bf3eede9d7291ce
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-02-19 09:26:12 +0100

    Lesson 702. Setting up the Starting Project

---
+++ animations/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  list = ['Milk', 'Sugar', 'Bread'];
+
+  onAdd(item: string) {
+    this.list.push(item);
+  }
+
+  onDelete(item: string) {
+    throw new Error('Method not implemented.');
+  }
+}
