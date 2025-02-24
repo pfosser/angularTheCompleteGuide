5aa707ce2bf01f4926ce2cdd6b9fe0cf7548162b
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-02-24 08:43:22 +0100

    Lesson 703. Animations Triggers and State

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -1,11 +1,32 @@
+import { state, style, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  animations: [
+    // Name totally up to the user: when in the DOM, it tells to apply the animation to it.
+    trigger('divState', [
+      state(
+        'normal',
+        style({
+          'background-color': 'red',
+          trasform: 'translateX(0)',
+        })
+      ),
+      state(
+        'highlighted',
+        style({
+          backgroundColor: 'blue',
+          trasform: 'translateX(100px)',
+        })
+      ),
+    ]),
+  ],
 })
 export class AppComponent {
+  state = 'normal'; // could be any kind of value
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAdd(item: string) {
