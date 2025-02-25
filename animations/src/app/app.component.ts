fd2c2e59b295c28991ab65682347b11c1d669d11
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-02-25 09:05:32 +0100

    Lesson 706. Advanced Transitions

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -18,7 +18,7 @@ import { Component } from '@angular/core';
         'normal',
         style({
           'background-color': 'red',
-          transform: 'translateX(0)',
+          transform: 'translateX(0)', // css setup here
         })
       ),
       state(
@@ -29,17 +29,52 @@ import { Component } from '@angular/core';
           transform: 'translateX(100px)',
         })
       ),
+      // transition('normal => highlighted', animate(300)),
+      // transition('highlighted => normal', animate(800)),
+      // Back and forth using the same animate function
+      transition('normal <=> highlighted', animate(300)),
+    ]),
+    trigger('wildState', [
+      state(
+        'normal',
+        style({
+          'background-color': 'red',
+          transform: 'translateX(0) scale(1)',
+        })
+      ),
+      state(
+        'highlighted',
+        style({
+          // must use the same naming style
+          'background-color': 'blue',
+          transform: 'translateX(100px) scale(1)',
+        })
+      ),
+      state(
+        'shrunken',
+        style({
+          'background-color': 'green',
+          transform: 'translateX(0) scale(0.5)',
+        })
+      ),
       transition('normal => highlighted', animate(300)),
       transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', animate(500)),
     ]),
   ],
 })
 export class AppComponent {
   state = 'normal'; // could be any kind of value
+  wildState = 'normal'; // could be any kind of value
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAnimate() {
     this.state = this.state === 'normal' ? 'highlighted' : 'normal';
+    this.wildState = this.wildState === 'normal' ? 'highlighted' : 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
   }
 
   onAdd(item: string) {
